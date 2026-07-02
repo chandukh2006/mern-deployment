a067b7ad905270ec231491134da65ce1c7058ff6
chandu <kchandu291@gmail.com>
2026-07-02 12:13:25 +0530

    Test automatic Jenkins deployment

--- mern/frontend/src/App.jsx
+++ mern/frontend/src/App.jsx
@@ -4,9 +4,14 @@ import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div className="w-full p-6">
+      <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
+        🚀 Jenkins CI/CD Working Successfully 🚀
+      </h1>
+
       <Navbar />
       <Outlet />
     </div>
   );
 };
+
 export default App;
