610e721c55bc252da9d97319042c86a82700ac7f
chandu <kchandu291@gmail.com>
2026-07-02 12:18:37 +0530

    Test GitHub webhook

--- mern/frontend/src/App.jsx
+++ mern/frontend/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="w-full p-6">
       <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
-        🚀 Jenkins CI/CD Working Successfully 🚀
+         Jenkins CI/CD Working Successfully 
       </h1>
 
       <Navbar />
