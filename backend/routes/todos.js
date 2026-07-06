b6c1a4b8756eee6a590f9b9e75036064a5181148
chandu <kchandu291@gmail.com>
2026-07-06 14:49:26 +0530

    changed frontend and backend

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Todo = require('../models/Todo');
 
-// GET all todos
+// get request from frontend
 router.get('/', async (req, res) => {
   try {
     const todos = await Todo.find().sort({ createdAt: -1 });
